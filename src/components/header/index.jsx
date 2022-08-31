a891100f1432e4d375ba51765c0b02cdaee05233
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-31 20:35:45 -0300

    creates header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import HeaderOptions from './headerOptions'
+import {
+    Box,
+    Button,
+    Container
+} from '@mui/material';
+import {
+    AttractionsOutlined,
+    DirectionsCarOutlined,
+    FlightOutlined,
+    HotelOutlined,
+    LocalTaxiOutlined
+} from '@mui/icons-material';
+
+const Header = () => {
+
+    const button = [
+        { title: 'Hospedagens', icon: HotelOutlined },
+        { title: 'Voos', icon: FlightOutlined },
+        { title: 'Aluguel de carros', icon: DirectionsCarOutlined },
+        { title: 'Atrações', icon: AttractionsOutlined },
+        { title: 'Táxis (aeroporto)', icon: LocalTaxiOutlined }
+    ];
+
+    const [selected, setSelected] = useState('Hospedagens')
+
+    return (
+        <Box sx={{ bgcolor: 'blue' }}>
+            <Container sx={{
+                display: { xs: 'none', md: 'flex' },
+                paddingY: '10px 0'
+            }}>
+                {button.map((item, key) => (
+                    <Button
+                        key={key}
+                        onClick={() => setSelected(item.title)}
+                        sx={{
+                            mr: 2,
+                            color: 'white',
+                            border: `${item.title === selected ? '1px solid white' : '1px solid transparent'}`,
+                            borderRadius: '25px',
+                            fontWeight: '700',
+                            textTransform: 'none'
+                        }}>
+                        <HeaderOptions Icon={item.icon} title={item.title} />
+                    </Button>
+                ))}
+            </Container>
+        </Box>
+    )
+}
+
+export default Header
