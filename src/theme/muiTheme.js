b0e03e3bb97d195cdd5aad7c8066d693b279b8d5
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-31 20:42:20 -0300

    creates o themeProvider

---
+++ src/theme/muiTheme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  shadows: 0,
+  typography: {
+    fontFamily: 'Poppins, sans-serif'
+  },
+  palette: {
+    background: {
+      default: '#FFFFFF'
+    }
+  },
+});
