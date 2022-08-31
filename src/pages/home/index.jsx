b2655f7ec1dc938e66fd5297ea7bce039b920eea
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-31 20:40:49 -0300

    creates home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../components/header'
+import Hero from '../../components/hero'
+import Navbar from '../../components/navbar'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <Hero />
+        </>
+    )
+}
+
+export default Home
