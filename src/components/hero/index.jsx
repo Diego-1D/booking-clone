5bed033af34f1ff390d6562ebe17f65d0f439fdf
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-05 20:48:12 -0300

    refactor

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,33 +1,29 @@
 import React from 'react'
 import {
-    Box,
-    Button,
     Container,
     Typography,
 } from '@mui/material';
 
 const Hero = () => {
-  
+
+    const container = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        paddingY: '4rem',
+        gap: '1rem',
+        color: 'white'
+    }
+
     return (
-        <Box sx={{ bgcolor: 'blue' }}>
-            <Container sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                p: '2rem',
-                gap: '2rem',
-                color: 'white'
-            }}>
-                <Typography variant='h4'>Uma vida inteira de descontos? Genial.</Typography>
-                <Typography variant='h6'>Seja recompensado por suas viagens –
-                    desbloqueie economias instantâneas de 10% ou
-                    mais com uma conta gratuita do Lamabooking
-                </Typography>
-                <Button sx={{ color: 'blue', bgcolor: 'white', height: '50px', width: '200px' }} >
-                    Entrar / Cadastrar
-                </Button>
-            </Container>
-        </Box>
+        <Container sx={container}>
+            <Typography
+                variant='h3'
+                sx={{ fontWeight: 'bold' }}>
+                Encontre a sua próxima estadia
+            </Typography>
+            <Typography variant='h5'>Pesquise ofertas em hotéis, casas e muito mais...</Typography>
+        </Container>
     )
 }
 
