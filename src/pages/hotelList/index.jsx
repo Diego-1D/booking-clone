448a830178dcc6031eecc97accc559d9d3d9de5e
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-13 19:42:36 -0300

    creates hotelList age

---
+++ src/pages/hotelList/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+import Header from '../../components/header'
+import Mail from '../../components/mail'
+import Footer from '../../components/footer'
+import SearchVertical from '../../components/searchVertical'
+import { Box, Container } from '@mui/material'
+import Cards from '../../components/cards'
+
+
+const HotelList = () => {
+
+  return (
+    <>
+      <Box sx={{ bgcolor: '#002984' }}>
+        <Navbar />
+        <Header />
+      </Box>
+      <Container sx={{ paddingY: '20px', display: 'flex', justifyContent: 'space-between' }} >
+        <SearchVertical />
+        <Cards/>
+      </Container>
+      <Mail />
+      <Footer />
+    </>
+  )
+}
+
+export default HotelList
