dd1f1be21b52ed4fb97960f6f63d23e7ca78dbdc
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-14 19:19:36 -0300

    refactor home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,39 @@
+import { Box, Container } from '@mui/material'
 import React from 'react'
+import Featured from '../../components/featured'
+import FeaturedProperties from '../../components/featuredProperties'
+import Footer from '../../components/footer'
 import Header from '../../components/header'
 import Hero from '../../components/hero'
+import Mail from '../../components/mail'
 import Navbar from '../../components/navbar'
+import PropertyList from '../../components/propertyList'
+import Search from '../../components/search'
 
 const Home = () => {
+
+    const container = {
+        display: 'flex',
+        flexDirection: 'column',
+        paddingY: '2rem',
+        gap: '1rem'
+    }
+
     return (
         <>
-            <Navbar />
-            <Header />
-            <Hero />
+            <Box sx={{ bgcolor: '#002984'}}>
+                <Navbar />
+                <Header />
+                <Hero />
+            </Box>
+            <Search />
+            <Container sx={container}>
+                <Featured />
+                <PropertyList />
+                <FeaturedProperties />
+            </Container>
+            <Mail />
+            <Footer />
         </>
     )
 }
