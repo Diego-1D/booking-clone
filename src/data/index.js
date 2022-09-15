43fc372b9158958af465ba11f759b9592e5cd7f2
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-15 18:18:53 -0300

    creates data

---
+++ src/data/index.js
@@ -0,0 +1,114 @@
+export const hotelList = [
+    {
+        img: 'https://cdn.pixabay.com/photo/2016/06/10/01/05/hotel-room-1447201__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Fabuloso',
+        assessments: '1.294',
+        location: '9,4',
+        execellent: '9,4'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Bom',
+        assessments: '694',
+        location: '5,4',
+        execellent: '8,1'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2018/06/14/21/15/bedroom-3475656__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Muito bom',
+        assessments: '1.094',
+        location: '9,1',
+        execellent: '8,3'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2021/12/11/07/59/hotel-6862159__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Fantástico',
+        assessments: '1.804',
+        location: '8,4',
+        execellent: '9,2'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2012/11/21/10/24/building-66789__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Fantástico',
+        assessments: '1.804',
+        location: '8,4',
+        execellent: '9,2'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2017/06/11/12/33/swimming-2392283__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Bom',
+        assessments: '1.804',
+        location: '8,4',
+        execellent: '9,2'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2018/02/24/17/17/window-3178666__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Fantástico',
+        assessments: '1.804',
+        location: '8,4',
+        execellent: '9,2'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2015/11/06/11/45/interior-1026452__340.jpg',
+        hotelname: 'Hotel Bowmann',
+        tags: [
+            'Asa norte, Brasília',
+            'Mostrar no mapa'
+        ],
+        distance: '0,8 km do centro',
+        description: 'Lorem ipsum dolor sit amet. 33 sint officiis non minima Quis sed placeat facilis aut beatae iusto? Quo voluptatem tempora et dolorum maiores et itaque repellendus. Rem galisum tempora ab incidunt Quis aut nostrum atque sit recusandae adipisci sed dolor nostrum aut minus molestiae qui ratione ipsam!',
+        status: 'Muito bom',
+        assessments: '1.294',
+        location: '9,4',
+        execellent: '9,4'
+    },
+]
