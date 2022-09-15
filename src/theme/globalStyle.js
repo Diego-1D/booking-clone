9b418ebb9af5fd3d3809e408b33f9895b3915056
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-15 18:21:31 -0300

    create button

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -11,3 +11,18 @@ export const CardTitle = styled("h6")(({  }) => ({
     fontWeight: 'bold',
     margin: 0
 }));
+
+
+export const Button = styled("button")(({  }) => ({
+    padding: '13px',
+    fontSize: '13px',
+    fontWeight: 'bold',
+    color: '#FFF',
+    backgroundColor: '#1976d2',
+    cursor: 'pointer',
+    border: 'none',
+
+    ':hover': {
+        backgroundColor: '#0153A4'
+    }
+}));
