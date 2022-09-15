2755e9cb6e781bebdd5202ee96bf92a3c11895c2
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-15 18:00:56 -0300

    refactor app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme/muiTheme";
 import Home from "./pages/home";
+import HotelList from "./pages/hotelList";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Home />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotellist" element={<HotelList />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
